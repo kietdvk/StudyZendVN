0cae8c65def856ed25778d3b014c022bbb22d946
Mr Key <kietdvkvn@gmail.com>
2023-08-06 19:38:00 +0700

    HomePage

---
+++ lib/consts/const.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+const TextStyle dStyleTextMain = TextStyle(
+  fontSize: 18.0,
+  color: Colors.black,
+);
+
+const TextStyle dStyleTextMainTitle = TextStyle(
+  fontSize: 25.0,
+  color: Colors.black,
+);
+
+const TextStyle dStyleTextSecond = TextStyle(
+  fontWeight: FontWeight.bold,
+  fontSize: 18,
+  color: Colors.blue,
+);
+
+//Padding
+const EdgeInsets dPadding = EdgeInsets.all(20.0);
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_study/consts/const.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -6,6 +7,24 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        leadingWidth: 210,
+        toolbarHeight: 65,
+        leading: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "ZendVN",
+              style: dStyleTextSecond,
+            ),
+            Text(
+              "Study Flutter",
+              style: dStyleTextMainTitle,
+            )
+          ],
+        ),
+        actions: [Icon(Icons.menu)],
+      ),
       body: Center(
         child: Text("Hello"),
       ),
